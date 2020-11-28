5c4a445be4c16ed0f59e23f906ec3720da73c836
Pierre Gomez <no@adresse.com>
2020-11-28 14:54:02 +0100

    clean uri file

--- src/fr/sorbonne_u/alasca/URIs.java
+++ src/fr/sorbonne_u/alasca/URIs.java
@@ -1,12 +1,8 @@
 package fr.sorbonne_u.alasca;
 
 public class URIs {
-<<<<<<< HEAD
 	public static final String FRIDGE_CONTROLLER_OUTBOUND_PORT_URI = "fridge-outbound-port-uri";
 	public static final String FRIDGE_INBOUND_PORT_URI = "fridge-inbound-port-uri";
 	public static final String TV_CONTROLLER_OUTBOUND_PORT_URI = "tv-outbound-port-uri";
 	public static final String TV_INBOUND_PORT_URI = "tv-inbound-port-uri";
-=======
-
->>>>>>> branch 'master' of https://github.com/pierregomez/HomeEnergyManager.git
 }
